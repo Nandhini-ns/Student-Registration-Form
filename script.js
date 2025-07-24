ba93b34d82954d8eb4e5d9b16a6c2ecd9a7959ab
Nandhini-ns <nandhininadimuthu99@gmail.com>
2025-07-24 16:43:50 +0530

    table page

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const apiUrl="https://6858d592138a18086dfc021c.mockapi.io/users";
+
+const form = document.getElementById("regidterform");
+if(form){
+   const name = document.getElementById("name").value.trim();
+   const last = document.getElementById("last").value.trim();
+   const dob = document.getElementById("dob").value.trim();
+   const gender = document.getElementById("input[name='gen']:checked")?.nextSibling
+}
